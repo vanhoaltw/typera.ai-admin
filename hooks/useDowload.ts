a62a4fea30fc3fd0b86759d17d8351cbf6fdb503
hoanguyen <vanhoaltw@gmail.com>
2024-01-25 22:17:01 +0700

    fix dowload

--- hooks/useDowload.ts
+++ hooks/useDowload.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { API_URI } from '../configs/env';
 import { getToken } from '../utils/session';
 import { useState } from 'react';
@@ -9,10 +10,19 @@ export const useDowload = () => {
         setLoading(true);
         const token = getToken();
         if (token) {
-            const anchor = document.createElement('a');
-            anchor.setAttribute('href', API_URI + '/download/' + researchId);
-            anchor.setAttribute('download', ' report.txt');
-            anchor.click();
+            const data = await axios.get(API_URI + '/download/' + researchId, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            if (data?.data) {
+                const blob = new Blob([data.data], { type: 'octet/stream' });
+                const url = URL.createObjectURL(blob);
+                const anchor = document.createElement('a');
+                anchor.setAttribute('href', url);
+                anchor.setAttribute('download', 'typera_report.txt');
+                anchor.click();
+            }
         }
         setLoading(false);
     };
